42ea030adfb2c091699af56438f0613b9fbcab25
AnkitKumar <roy.ankit5008@gmail.com>
2024-01-21 18:43:14 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,103 +2,93 @@ import React from "react";
 import styles from "@/home.module.css";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
 const page = () => {
-    return (
-        <div className={`${styles.background}`}>
-            <div className="flex h-[100vh] w-full items-center justify-center">
-                <Tabs defaultValue="login" className="w-[400px]">
-                    <TabsList className="grid w-full grid-cols-2">
-                        <TabsTrigger value="login">Login</TabsTrigger>
-                        <TabsTrigger value="signup">Signup</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="login">
-                        <Card>
-                            <CardHeader>
-                                <CardTitle className="text-center">
-                                    Login
-                                </CardTitle>
-                            </CardHeader>
-                            <CardContent className="space-y-2">
-                                <div className="space-y-1">
-                                    <Label htmlFor="email">Email Address</Label>
-                                    <Input
-                                        id="email"
-                                        type="text"
-                                        placeholder="Enter your email address"
-                                    />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="password">Password</Label>
-                                    <Input
-                                        id="password"
-                                        type="password"
-                                        placeholder="Enter password"
-                                    />
-                                </div>
-                            </CardContent>
-                            <CardFooter>
-                                <Button>Login</Button>
-                            </CardFooter>
-                        </Card>
-                    </TabsContent>
-                    <TabsContent value="signup">
-                        <Card>
-                            <CardHeader>
-                                <CardTitle className="text-center">
-                                    Signup
-                                </CardTitle>
-                            </CardHeader>
-                            <CardContent className="space-y-2">
-                                <div className="space-y-1">
-                                    <Label htmlFor="name">Name</Label>
-                                    <Input
-                                        id="name"
-                                        type="text"
-                                        placeholder="Enter your name"
-                                    />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="email">Email</Label>
-                                    <Input
-                                        id="email"
-                                        type="text"
-                                        placeholder="Enter your email address"
-                                    />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="password">Password</Label>
-                                    <Input
-                                        id="password"
-                                        type="password"
-                                        placeholder="Enter your password"
-                                    />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="picture">
-                                        Upload your picture
-                                    </Label>
-                                    <Input id="picture" type="file" />
-                                </div>
-                            </CardContent>
-                            <CardFooter>
-                                <Button>Save password</Button>
-                            </CardFooter>
-                        </Card>
-                    </TabsContent>
-                </Tabs>
-            </div>
-        </div>
-    );
+  return (
+    <div className={`${styles.background}`}>
+      <div className="flex h-[100vh] w-full items-center justify-center">
+        <Tabs defaultValue="login" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="signup">Signup</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-center">Login</CardTitle>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="email">Email Address</Label>
+                  <Input
+                    id="email"
+                    type="text"
+                    placeholder="Enter your email address"
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="Enter password"
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Login</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="signup">
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-center">Signup</CardTitle>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="name">Name</Label>
+                  <Input id="name" type="text" placeholder="Enter your name" />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="text"
+                    placeholder="Enter your email address"
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="Enter your password"
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="picture">Upload your picture</Label>
+                  <Input id="picture" type="file" />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Save password</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default page;
